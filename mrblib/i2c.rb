64f26d434ce860c4adda147130460c38ae2d1366
kishima <kishima7@gmail.com>
2025-06-24 00:07:33 +0900

    impl read

--- mrblib/i2c.rb
+++ mrblib/i2c.rb
@@ -2,42 +2,45 @@ module ESP32
   class I2C
     include Constants
 
-    def initialize(port, options={})
+    def initialize()
+      # @port = port
+      # @scl = options[:scl] || SCL0
+      # @scl_pullup = options[:scl_pullup] || true
+      # @sda = options[:sda] || SDA0
+      # @sda_pullup = options[:sda_pullup] || true
+      # @freq = options[:freq] || 100000
+      # puts "A #{@port}, #{@mode}, #{@scl}, #{@scl_pullup}, #{@sda}, #{@sda_pullup}, #{@freq}"\
+    end
+
+    def test(arg)
+      puts "test #{arg}"
+    end
+
+    def init(port, options={})
+      puts "init"
+      @mode = MASTER
+      #SLAVE not supported
       @port = port
       @scl = options[:scl] || SCL0
       @scl_pullup = options[:scl_pullup] || true
       @sda = options[:sda] || SDA0
       @sda_pullup = options[:sda_pullup] || true
       @freq = options[:freq] || 100000
-      puts "A #{@port}, #{@mode}, #{@scl}, #{@scl_pullup}, #{@sda}, #{@sda_pullup}, #{@freq}"\
-    end
-
-    def set_default_val(port, options={})
-      @port = port
-      @scl = options[:scl] || SCL0
-      @scl_pullup = options[:scl_pullup] || true
-      @sda = options[:sda] || SDA0
-      @sda_pullup = options[:sda_pullup] || true
-      @freq = options[:freq] || 100000
-      puts "C #{@port}, #{@mode}, #{@scl}, #{@scl_pullup}, #{@sda}, #{@sda_pullup}, #{@freq}"\
-    end
-
-    def init(mode=MASTER)
-      raise ArgumentError.new unless [MASTER,SLAVE].include?(mode)
-      raise NotImplementedError.new unless mode.eql?(MASTER)
-      @mode = mode
-      puts "B #{@port}, #{@mode}, #{@scl}, #{@scl_pullup}, #{@sda}, #{@sda_pullup}, #{@freq}"
+      puts "init #{@port}, #{@mode}, #{@scl}, #{@scl_pullup}, #{@sda}, #{@sda_pullup}, #{@freq}"
       _init(@port, @mode, @scl, @scl_pullup, @sda, @sda_pullup, @freq)
     end
 
     def scan()
+      puts "scan"
       raise I2CError.new unless @mode.eql?(MASTER)
       ary = []
       (0x08..0x77).each{ |addr|
+        puts "addr: #{addr}"
         ary << addr if send("", addr) == 0
       }
       ary
     end
+
     def ready?(addr)
       raise I2CError.new unless @mode.eql?(MASTER)
       return send("", addr) == 0 ? true : false
--- src/i2c.c
+++ src/i2c.c
@@ -8,27 +8,35 @@
 #define TAG "i2c"
 #include "driver/i2c.h"
 
-#define E_I2C_ERROR (mrb_class_get(mrb, "I2CError"))
+#define I2C_MASTER_NUM              0        /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_TX_BUF_DISABLE   0        /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0        /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS       1000
 
 static mrb_value
 mrb_esp32_i2c_init(mrb_state *mrb, mrb_value self)
 {
   ESP_LOGI(TAG, "mrb_esp32_i2c_init");
+  
   mrb_int mode, port, scl, sda, freq;
-  mrb_bool scl_pullup, sda_pullup;
-  i2c_config_t conf;
-
+  mrb_int scl_pullup, sda_pullup;
   mrb_get_args(mrb, "iiibibi", &port, &mode, &scl, &scl_pullup, &sda, &sda_pullup, &freq);
 
-  conf.mode = mode;
-  conf.scl_io_num = scl;
-  conf.sda_io_num = sda;
-  conf.scl_pullup_en = scl_pullup;
-  conf.sda_pullup_en = sda_pullup;
-  conf.master.clk_speed = freq;
+  int i2c_master_port = I2C_MASTER_NUM;
+  //GPIO_PULLUP_DISABLE 0
+  //GPIO_PULLUP_ENABLE 1
 
-  i2c_param_config(port, &conf);
-  i2c_driver_install(port, mode, 0, 0, 0);
+  i2c_config_t conf = {
+      .mode = I2C_MODE_MASTER,
+      .sda_io_num = sda,
+      .scl_io_num = scl,
+      .sda_pullup_en = sda_pullup,
+      .scl_pullup_en = scl_pullup,
+      .master.clk_speed = freq,
+  };
+
+  i2c_param_config(I2C_MASTER_NUM, &conf);
+  i2c_driver_install(I2C_MASTER_NUM, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
 
   return self;
 }
@@ -68,28 +76,17 @@ mrb_esp32_i2c_send(mrb_state *mrb, mrb_value self) {
 
 static mrb_value
 mrb_esp32_i2c_recv(mrb_state *mrb, mrb_value self) {
-    mrb_value port;
-    mrb_int addr, len;
-    i2c_cmd_handle_t cmd;
+    mrb_int dev_addr, reg_addr, len;
     esp_err_t err;
     char *buf;
     mrb_value result;
 
-    mrb_get_args(mrb, "ii", &addr, &len);
-    port = mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@port"));
+    mrb_get_args(mrb, "iii", &dev_addr, &reg_addr, &len);
 
     buf = (char*)mrb_malloc(mrb, len);
     
-    cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, (addr << 1) | I2C_MASTER_READ, 1);
-    if (len > 1) {
-        i2c_master_read(cmd, (uint8_t*)buf, len - 1, I2C_MASTER_ACK);
-    }
-    i2c_master_read_byte(cmd, (uint8_t*)&buf[len - 1], I2C_MASTER_NACK);
-    i2c_master_stop(cmd);
-    err = i2c_master_cmd_begin(mrb_fixnum(port), cmd, 1000 / portTICK_PERIOD_MS);
-    i2c_cmd_link_delete(cmd);
+    uint8_t reg_addr_8 = (uint8_t)reg_addr;
+    err = i2c_master_write_read_device(I2C_MASTER_NUM, (uint8_t)dev_addr, &reg_addr_8, 1, (uint8_t*)buf, len, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
 
     if (err == ESP_OK) {
         result = mrb_str_new(mrb, buf, len);
